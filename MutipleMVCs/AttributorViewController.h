e54026d460c66e4d80a82dc5f5e5ee3f84f939b2
Mocca Yang <tiffany12310@gmail.com>
2017-04-20 16:41:56 +0800

    Init commit

---
+++ MutipleMVCs/AttributorViewController.h
@@ -0,0 +1,13 @@
+//
+//  AttributorViewController.h
+//  MutipleMVCs
+//
+//  Created by Mocca Yang on 2017/4/20.
+//  Copyright © 2017年 Mocca Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributorViewController : UIViewController
+
+@end

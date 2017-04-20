d1c844d626dbeede0cb0d0ea7b27ca3543ce59a5
Mocca Yang <tiffany12310@gmail.com>
2017-04-20 15:54:19 +0800

    Initial Commit

---
+++ MutipleMVCs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MutipleMVCs
+//
+//  Created by Mocca Yang on 2017/4/20.
+//  Copyright © 2017年 Mocca Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
